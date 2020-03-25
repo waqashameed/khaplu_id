7d61660f2a6959af068109d3de5992517f0ad616
Waqas <m_waqas_hameed@hotmail.com>
2020-03-25 14:30:07 +0500

    Converted to Stateful Widget and added a button to increase player level

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,14 @@ void main() => runApp(MaterialApp(
       home: KhapluCard(),
     ));
 
-class KhapluCard extends StatelessWidget {
+class KhapluCard extends StatefulWidget {
+  @override
+  _KhapluCardState createState() => _KhapluCardState();
+}
+
+class _KhapluCardState extends State<KhapluCard> {
+  int currentLevel = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,6 +21,15 @@ class KhapluCard extends StatelessWidget {
           child: Text('Khalpu ID Card'),
         ),
         backgroundColor: Colors.grey[850],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.grey[800],
+        child: Icon(Icons.add),
+        onPressed: () {
+          setState(() {
+            currentLevel += 1;
+          });
+        },
       ),
       body: Padding(
         padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
@@ -47,9 +63,9 @@ class KhapluCard extends StatelessWidget {
                 fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(height: 30.0),
+            SizedBox(height: 25.0),
             Text(
-              'Position',
+              'POSITION',
               style: TextStyle(
                 color: Colors.grey,
                 letterSpacing: 2.0,
@@ -65,7 +81,27 @@ class KhapluCard extends StatelessWidget {
                 fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(height: 30.0),
+            SizedBox(height: 25.0),
+            Text(
+              'CURRENT LEVEL',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              '$currentLevel',
+              style: TextStyle(
+                color: Colors.amber,
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 25.0,
+            ),
             Row(
               children: <Widget>[
                 Icon(
